13dc8934241e08f8a6c378bee52be1b934fa9ad4
Ashish <ashish@gmail.com>
2026-03-10 10:17:28 +0530

    add include

--- 2a.js
+++ 2a.js
@@ -1,9 +1,12 @@
 let a=[1,2,true,'hi',3.2]
-a.pop();
+a.pop();//use to remove one element from last
 console.log(a);
-a.push(999999);
+a.push(999999);//use to add on element from last
 console.log(a);
-a.shift();
+a.shift();//use to remove on element from first
 console.log(a);
-a.unshift('hello');
+a.unshift('hello');//use to add on element from first
 console.log(a);
+a.includes(a);//to checkout if element is included in array or not(true or false)
+console.log(a.includes(2));
+console.log(a.includes(5));

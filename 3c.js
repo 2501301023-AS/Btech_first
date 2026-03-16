3de8040a73f2201b6c79b0ea2794eaf02c98b8cc
Ashish <ashish@gmail.com>
2026-03-16 10:41:21 +0530

    MULTI AND DIV AND SUB

--- 3c.js
+++ 3c.js
@@ -1,4 +1,14 @@
+// MODULE START:-
 // first.js
 export const add=(a,b)=>{
     return a+b;
+}
+export const sub=(a,b)=>{
+    return a-b;
+}
+export const multi=(a,b)=>{
+    return a*b;
+}
+export const div=(a,b)=>{
+    return a/b;
 }
--- 3d.js
+++ 3d.js
@@ -1,5 +1,11 @@
 // second.js
-import{add}from "./3c.js";
+import{add,sub,multi,div}from "./3c.js";
 let b=add(7,8);
 console.log(b);
 // we only run second
+let c=sub(8,7);
+console.log(c);
+let d=multi(8,7);
+console.log(d);
+let e=div(14,7);
+console.log(e);
